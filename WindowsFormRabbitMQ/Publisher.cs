745ed74107280812217c599488c932981fe61df8
zeynep öztürkmen <zynpoztrkmn@hotmail.com>
2020-06-06 18:47:02 +0300

    Add project files.

---
+++ WindowsFormRabbitMQ/Publisher.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormRabbitMQ
+{
+    public class Publisher
+    {
+
+        private readonly RabbitMQService _rabbitMQService;
+
+        public Publisher(string queueName, string message)
+        {
+            _rabbitMQService = new RabbitMQService();
+
+            using (var connection = _rabbitMQService.GetRabbitMQConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queueName, false, false, false, null);
+
+                    channel.BasicPublish("", queueName, null, Encoding.UTF8.GetBytes(message));
+
+                    //Console.WriteLine("{0} queue'su üzerine, \"{1}\" mesajı yazıldı.", queueName, message);
+                }
+            }
+        }
+
+    }
+}
---
+++ WindowsFormRabbitMQ/RabbitMQService.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormRabbitMQ
+{
+    public class RabbitMQService
+    {
+        // localhost üzerinde kurulu olduğu için host adresi olarak bunu kullanıyorum.
+        private readonly string _hostName = "localhost";
+
+        public IConnection GetRabbitMQConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                // RabbitMQ'nun bağlantı kuracağı host'u tanımlıyoruz. Herhangi bir güvenlik önlemi koymak istersek, Management ekranından password adımlarını tanımlayıp factory içerisindeki "UserName" ve "Password" property'lerini set etmemiz yeterlidir.
+                HostName = _hostName
+            };
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+}
